6bc8303b39dfb87ccdcca2deec18e7af4f6931f2
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-10-02 22:07:18 +0200

    change C files and compile them once again

--- C/HelloWorld.c
+++ C/HelloWorld.c
@@ -12,9 +12,9 @@ int main()
   printf("\nEnter # of columns ");
   scanf("%d",&columns);
   scanf("%c");
-  printf("\nEnter a symbol to use\n");
+  printf("\nEnter a symbol to use");
   scanf("%c", &symbol);
-
+  printf("\n");
     for (int height = 0; height <= rows;height++)
     {
         for(int widht = 0;widht <= columns;widht++ )
